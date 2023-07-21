3ef90ff8e659ffc2a4ab535219c04196b7b086f7
sidethree_kfujimoto <kfujimoto@sidethree.co.jp>
2023-07-21 15:10:14 +0900

    no message

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `test siter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `test site`,
+    description: `test site`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
